749d0a00370c2de51c2f749e524c6865a86ad091
Marcin Maciejewski <kontakt@ulanelectronics.pl>
2020-06-16 21:21:43 +0200

    [Setup] for ads from Adsterra

---
+++ config/ads.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'ad600x160' => env('AD_600_160', null), 
+    'ad90x728' => env('AD_90_728', null),
+    'ad250x300' => env('AD_250_300', null)
+];
--- resources/views/redirect.blade.php
+++ resources/views/redirect.blade.php
@@ -5,12 +5,38 @@
     <div class="columns is-vcentered">
         <div class="column is-2">
             <figure class="image">
+                @if(config('app.env') == 'production')
+                <script type="text/javascript">
+                    atOptions = {
+                        'key': '{{config("ads.ad600x160")}}',
+                        'format': 'iframe',
+                        'height': 600,
+                        'width': 160,
+                        'params': {}
+                    };
+                    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.bestdisplayformats.com/{{config("ads.ad600x160")}}/invoke.js"></scr' + 'ipt>');
+                </script>
+                @else
                 <img src="https://dummyimage.com/160x600/000/fff" style="display:inline-block;width:160px;height:600px">
+                @endif
             </figure>
         </div>
         <div class="column is-6">
             <figure class="image">
+                @if(config('app.env') == 'production')
+                <script type="text/javascript">
+                    atOptions = {
+                        'key': '{{config("ads.ad90x728")}}',
+                        'format': 'iframe',
+                        'height': 90,
+                        'width': 728,
+                        'params': {}
+                    };
+                    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.bestdisplayformats.com/{{config("ads.ad90x728")}}/invoke.js"></scr' + 'ipt>');
+                </script>
+                @else
                 <img src="https://dummyimage.com/728x90/000/fff" style="display:inline-block;width:728px;height:90px">
+                @endif
             </figure>
             <h1 class="title is-2">
                 You are being redirected <br>in <span id="time-remaining">5</span> seconds
@@ -30,15 +56,54 @@
             </p>
             <br>
             <figure class="image">
+                @if(config('app.env') == 'production')
+                <script type="text/javascript">
+                    atOptions = {
+                        'key': '{{config("ads.ad90x728")}}',
+                        'format': 'iframe',
+                        'height': 90,
+                        'width': 728,
+                        'params': {}
+                    };
+                    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.bestdisplayformats.com/{{config("ads.ad90x728")}}/invoke.js"></scr' + 'ipt>');
+                </script>
+                @else
                 <img src="https://dummyimage.com/728x90/000/fff" style="display:inline-block;width:728px;height:90px">
+                @endif
             </figure>
         </div>
         <div class="column is-4">
             <figure class="image">
+                @if(config('app.env') == 'production')
+                <script type="text/javascript">
+                    atOptions = {
+                        'key': '{{config("ads.ad250x300")}}',
+                        'format': 'iframe',
+                        'height': 250,
+                        'width': 300,
+                        'params': {}
+                    };
+                    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.bestdisplayformats.com/{{config("ads.ad250x300")}}/invoke.js"></scr' + 'ipt>');
+                </script>
+                @else
                 <img src="https://dummyimage.com/300x250/000/fff" style="display:inline-block;width:300px;height:250px">
+                @endif
             </figure>
             <figure class="image">
+                @if(config('app.env') == 'production')
+                <script type="text/javascript">
+                    atOptions = {
+                        'key': '{{config("ads.ad250x300")}}',
+                        'format': 'iframe',
+                        'height': 250,
+                        'width': 300,
+                        'params': {}
+                    };
+                    document.write('<scr' + 'ipt type="text/javascript" src="http' + (location.protocol === 'https:' ? 's' : '') + '://www.bestdisplayformats.com/{{config("ads.ad250x300")}}/invoke.js"></scr' + 'ipt>');
+                </script>
+                @else
                 <img src="https://dummyimage.com/300x250/000/fff" style="display:inline-block;width:300px;height:250px">
+                @endif
             </figure>
         </div>
     </div>
